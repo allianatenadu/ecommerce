7daf96ba4eb4d4769184a5aafd55b22eae820423
Aliana Tenadu <alianatenadu@gmail.com>
2025-02-03 14:56:44 +0000

    again

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-black text-white py-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 Your Venderious Site. All Rights Reserved.</p>
+        <p>&copy; 2025 Your Venderious Sites. All Rights Reserved.</p>
         <p>Made with Little_pixy.❤️ in Ghana.</p>
       </div>
     </footer>
